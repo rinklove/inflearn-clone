687bd9749fc5611d1344bac381cc3139ea6ba1c1
rinklove <88640680+rinklove@users.noreply.github.com>
2023-11-06 19:14:48 +0900

    chore: setting inflearn-clone

---
+++ inflearn.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
+
+const search = () => {
+    const searchInput = document.getElementById("search-input");
+    const keyword = searchInput.value;
+    
+    const getLink = `https://www.inflearn.com/courses?s=${keyword}`;
+    location.href = getLink;
+}
---
+++ login.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+    document.get
+})
+
+const login = () => {
+    const memberId = document.getElementById("member-id");
+
+    const result = alert(`${memberId.value}님 환영합니다~`);
+    
+}
